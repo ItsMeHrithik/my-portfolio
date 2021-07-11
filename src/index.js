af7d86fbae449540b715f89d9e2c148c49f5ddba
hrithik_bt18 <hrithik_bt18@iiitkalyani.ac.in>
2021-07-11 13:14:38 +0530

    portfolio setuo

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const Portfolio = () => {
+  return (
+    <div>
+      <App />
+    </div>
+  );
+};
+
+ReactDOM.render(<Portfolio />, document.getElementById("root"));
