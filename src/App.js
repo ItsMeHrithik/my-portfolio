d2d7ea036f9056232741d258d9f1a761247aaf87
hrithik_bt18 <hrithik_bt18@iiitkalyani.ac.in>
2021-07-12 12:53:39 +0530

    Added Css to the about section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AboutUs from "./pages/AboutUs";
+import GlobalStyle from "./components/GlobalStyle";
 
 const App = () => {
   return (
     <div>
       <h1>Welcome to my portfolio</h1>
-        <AboutUs />
+      <GlobalStyle />
+      <AboutUs />
     </div>
   );
 };
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,31 +1,66 @@
 import React from "react";
-import HomeImage from "../img/home-banner.jpg";
+import HomeImage from "../img/home-banner.jpeg";
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div className="description">
-      <div className="title">
-        <div className="hide">
-          <h2>We work to make</h2>
+    <About>
+      <Description>
+        <div className="title">
+          <Hide>
+            <h2>We work to make</h2>
+          </Hide>
+          <Hide>
+            <h2>
+              your <span> dreams</span> come
+            </h2>
+          </Hide>
+          <Hide>
+            <h2>true.</h2>
+          </Hide>
+          <p>
+            Contact us for making you e-commerce businees grow to the next level
+            with all support
+          </p>
+          <button>Contact Us</button>
         </div>
-        <div className="hide">
-          <h2>
-            your <span> dreams</span> come
-          </h2>
-        </div>
-        <div className="hide">
-          <h2>true.</h2>
-        </div>
-        <p>
-          Contact us for making you e-commerce businees grow to the next level
-          with all support
-        </p>
-        <button>Contact Us</button>
-      </div>
-      <div className="image">
+      </Description>
+      <Image>
         <img src={HomeImage} alt="Webdevloper" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
+
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow : hidden;
+  img {
+    width: 120%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+overflow : hidden;
+`;
+
 export default AboutSection;
